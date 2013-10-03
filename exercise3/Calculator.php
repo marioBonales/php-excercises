933aafe260dd37a89afc81784b9702f02e7fb40f
Mario Bonales <mario.bonales@gmail.com>
2013-10-03 09:46:38 -0700

    Completed Excercise 3 Calculator

--- exercise3/Calculator.php
+++ exercise3/Calculator.php
@@ -1,7 +1,15 @@
 <?
 class Calculator{
-    public static function add(){}
-    public static function substract(){}
-    public static function multiply(){}
-    public static function divide(){}
+    public static function add($number1,$number2){
+        return $number1 + $number2;
+    }
+    public static function substract($number1,$number2){
+        return $number1 - $number2;
+    }
+    public static function multiply($number1,$number2){
+        return $number1 * $number2;
+    }
+    public static function divide($number1,$number2){
+        return $number1 / $number2;
+    }
 }
