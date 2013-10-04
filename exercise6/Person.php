471dc57ac9b19340909ecad9f282050b7d90031a
Mario Bonales <mario.bonales@gmail.com>
2013-10-04 09:32:32 -0700

    Model and Model Unit tests beginning

---
+++ exercise6/Person.php
@@ -0,0 +1,61 @@
+<?
+class Person {
+    public $id, $name, $last_name,$age, $email, $telephone;
+    protected $db;
+
+    public function __construct($data,$db=null){
+        if($db == null){
+            $this->db = new PDO('mysql:host=localhost;dbname=exercise;','exercise','exercise9');
+        }else{
+            $this->db = $db;
+        }
+        $this->name = $data['name'];
+        $this->last_name = $data['last_name'];
+        $this->age = $data['age'];
+        $this->email = $data['email'];
+        $this->telephone = $data['telephone'];
+    }
+
+    public static function fetch($id,$db=null){
+        if($db == null){
+            $db = new PDO('mysql:host=localhost;dbname=exercise;','exercise','exercise9');
+        }
+        $statement = $db->prepare("SELECT * FROM person WHERE id= :id LIMIT 1");
+        $statement->bindParam(":id",$id);
+        $statement->execute();
+        if( $person_data = $statement->fetch()){
+            return new Person($person_data,$db);
+        }
+        return null;
+
+    }
+
+    public function save(){
+        if(!$this->id){
+            $statement = $this->db->prepare(<<<SQL
+            INSERT INTO person (name, last_name, age, email, telephone)
+            VALUES (:name, :last_name,:age,:email, :telephone)
+SQL
+            );
+        }else{
+            $statement = $this->db->prepare(<<<SQL
+            UPDATE person set name = :name, last_name = :last_name, age = :age,
+            email = :email, telephone = :telephone
+            WHERE id = :id
+SQL
+            );
+            $statement->bindParam(":id", $this->id);
+        }
+            $statement->bindParam(":name", $this->name);
+            $statement->bindParam(":last_name",$this->last_name);
+            $statement->bindParam(":age", $this->age);
+            $statement->bindParam(":email", $this->email);
+            $statement->bindParam(":telephone", $this->telephone);
+            $statement->execute();
+            if(!$this->id && $success = $statement->errorCode() == 0){
+                $this->id = $this->db->lastInsertId();
+            }
+            return $success;
+    }
+
+}
---
+++ exercise6/test/PersonTest.php
@@ -0,0 +1,22 @@
+<?
+include("../Person.php");
+class PersonTest extends PHPUnit_Framework_TestCase{
+
+    protected function setUp(){
+        $data = array(
+            "name" => "Mario",
+            "last_name" => "Bonales",
+            "age" => 24,
+            "email" => "me@mariobonales.com",
+            "telephone" => "1111111111"
+        );
+        $this->db = $this->db = new PDO('mysql:host=localhost;dbname=exercise_test;','exercise_test','exercise9');
+        $this->person = new Person($data,$this->db);
+    }
+
+    public function testPersonCanBeSaved(){
+        $this->assertTrue($this->person->save());
+        $this->assertTrue(Person::fetch($this->person->id,$this->db) instanceof Person );
+    }
+
+}
